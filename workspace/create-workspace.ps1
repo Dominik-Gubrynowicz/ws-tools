689cf820d0a941b56db442056490a956ed66cfdf
Dominik Gubrynowicz <dominik.gubrynowicz@gmail.com>
2024-09-06 18:26:05 +0200

    Feat: get instance ip and configure etc hosts

--- workspace/create-workspace.ps1
+++ workspace/create-workspace.ps1
@@ -1,7 +1,10 @@
 #Poweshell script
+$homedir = $env:USERPROFILE
+# Create ssh directory if not exists
+New-Item -Path "$homedir\.ssh" -ItemType Directory
 
 # Create ssh key
-ssh-keygen -t rsa -b 4096 -f id_rsa
+ssh-keygen -t rsa -b 4096 -f "$homedir\.ssh\id_rsa"
 
 $region = "us-east-1"
 $subnet = "subnet-04fd67a27d2b40b4f"
@@ -17,22 +20,38 @@ $ami = & aws ec2 describe-images `
 echo $ami
 
 # Remplace the region in the user-data.sh file powrshell
-$keyPath = "id_rsa.pub"
+$keyPath = "$homedir\.ssh\id_rsa.pub"
 $filePath = "user-data.sh"
 $filePath2 = "data.sh"
 $oldText = "SSH_PUBLIC_KEY"
 $newText = Get-Content $keyPath
 
-echo $newText
-
 (Get-Content $filePath).Replace($oldText, $newText) | Set-Content $filePath2
 
 # Create ec2 instance
-aws ec2 run-instances `
+$instance_id = & aws ec2 run-instances `
     --image-id $ami `
     --count 1 `
     --instance-type t3.medium `
     --region $region `
     --subnet-id $subnet `
     --user-data file://$filePath2 `
-    --associate-public-ip-address
+    --associate-public-ip-address `
+    --query 'Instances[0].InstanceId' `
+    --output text
+
+echo $instance_id
+
+sleep 10
+
+# Get public IP
+$public_ip = & aws ec2 describe-instances `
+    --instance-ids $instance_id `
+    --query 'Reservations[0].Instances[0].PublicIpAddress' `
+    --output text `
+    --region $region
+
+
+
+# Create .ssh config file with ssh configuration
+New-Item $homedir\.ssh\config -ItemType File -Force -Value "Host $public_ip`n    User ubuntu`n    IdentityFile $home\.ssh\id_rsa"
